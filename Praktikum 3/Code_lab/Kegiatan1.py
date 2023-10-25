61d76a275337f21739ae132dd72262e8b870e56c
Rafidhiya Bagus <yoviyovi47@gmail.com>
2023-10-25 17:29:16 +0700

    modul 3

---
+++ Praktikum 3/Code_lab/Kegiatan1.py
@@ -0,0 +1,28 @@
+data = ["3 minggu 3 hari 7 jam 21 menit",
+         "5 minggu 5 hari 8 jam 11 menit",
+         "7 minggu 1 hari 5 jam 33 menit"]
+
+def convert_to_minutes(weeks,days,hours,minutes): #Convert data dari Menit, Jam, Hari, Minggu
+    return weeks * 7 *24 * 60 + days * 24 * 60 + hours * 60 + minutes
+
+def curried_coverter(weeks): #Currying
+    def inner_curried(days):
+        def inner_inner_curried(hours):
+            def final_curried(minutes):
+                return convert_to_minutes(weeks,days,hours,minutes) #convert ke dalam Menit
+            return final_curried
+        return inner_inner_curried
+    return inner_curried
+
+outputData = []
+for item in data:
+    parts = item.split()
+    weeks = int(parts[0])
+    days = int(parts[2])
+    hours = int(parts[4])
+    minutes = int(parts[6])
+
+    coverted_values = curried_coverter(weeks)(days)(hours)(minutes)
+    outputData.append(coverted_values)
+
+print("OutputData = " ,outputData)
---
+++ Praktikum 3/Code_lab/Kegiatan3.py
@@ -0,0 +1,21 @@
+# Data list seperti pada kegiatan 1 modul 1
+random_list = [3.1, 2.7, 5.5, 105, 737, 412, 'Hello', 'Python', 'world', 'AI']
+
+# Filter() untuk memisahkan nilai float, int, dan string
+# Dapat dikatakan menerapkan HoF tipe function as parameter karena ada filter() yang argumenya adalah fungsi dalam bentuk lambda
+float_data = list(filter(lambda x: isinstance(x, float), random_list))
+int_data = list(filter(lambda x: isinstance(x, int), random_list))
+string_data = list(filter(lambda x: isinstance(x, str), random_list))
+
+# Dapat dikatakan menerapkan HoF tipe function as parameter karena ada map() yang argumenya adalah fungsi dalam bentuk lambda
+# Map() untuk memetakan nilai int menjadi satuan, puluhan, dan ratusan
+intdata_mapped = list(map(lambda x: {'ratusan': x // 100, 'puluhan': (x % 100) // 10, 'satuan': x % 10}, int_data))
+
+# Output
+print("Data Float:")
+print(float_data)
+print("Data Int:")
+for item in intdata_mapped:
+    print(item)
+print("Data String:")
+print(string_data)
